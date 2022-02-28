36f5cac68d060f19256f70d3ca418a000aaf7ec6
Trevor Billet <98602126+TrevorBillet@users.noreply.github.com>
2022-02-27 19:51:37 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,75 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        for (int x = 0; x <= 30; x++)
+        {
+            System.out.printf("%4d", x);
+        }
+        System.out.println();
+
+        for (int x = 30; x >= 0; x--)
+        {
+            System.out.printf("%4d", x);
+        }
+        System.out.println();
+
+        for (int x = 0; x <= 18; x+= 3)
+        {
+            System.out.printf("%4d", x);
+        }
+
+        System.out.println();
+
+        for (int x = 10; x >= 0; x-= 2)
+        {
+            System.out.printf("%4d", x);
+        }
+
+        System.out.println();
+
+        for (int row = 0; row <= 5; row++)
+        {
+            for (int col = 0; col <= 5; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int row = 0; row <= 5; row++)
+        {
+            for (int col = 0; col <= row; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int row = 5; row >= 0; row--)
+        {
+            for (int col = 0; col <= row; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int row = 0; row <= 5; row++)
+        {
+            for (int col = 0; col <= row; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
